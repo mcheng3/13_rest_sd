ae18ee818ed8791b6b8aabef3fb5e5b27659f58d
mcheng3 <michael.cheng65@gmail.com>
2017-11-09 21:58:24 -0500

    Finish nasaImage.py

--- nasaImage.py
+++ nasaImage.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import urllib2
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template("nasaimg.html")
+	u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=0XyeqBRNTN0NQCOWgHjq7K9GH6OAXJZoYjN7uE8Q")
+	jsonString = u.read()
+	d = json.loads(jsonString)
+	return render_template("nasaimg.html", img = d["hdurl"], desc = d["explanation"], date = d["date"])
 
 if __name__ == "__main__":
 	app.debug = True
